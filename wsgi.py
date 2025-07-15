bd79836f00fd8fce7e32cb67f516d2270cbbb483
Asad Mehmood <asadmehmood96@gmail.com>
2025-07-15 22:58:01 +0500

    issue resolved

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from app import app
 # Import routes after app is created (avoids circular import)
 from app.router import routes
@@ -6,7 +6,7 @@ from app.router import project_routes
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5050, debug=True)
-=======
+    
 from app import app
 # Import routes after app is created (avoids circular import)
 from app.router import routes
@@ -14,4 +14,4 @@ from app.router import project_routes
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5050, debug=True)
->>>>>>> 7ca1db230b61a2d4bb7786040d65f0c1ea2d49b3
+
